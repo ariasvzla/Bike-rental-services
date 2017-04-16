b0d56cbc45edadd948b9e326e3b34a576d97611d
Elianny Arias <arias.vzla@gmail.com>
2017-04-16 16:57:32 +0100

    contacts added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :profiles
 
 devise_for :users, controllers: {
@@ -25,6 +26,7 @@ get '/adminpage' => 'profiles#adminpage'
 get '/pricing' => 'sites#pricing'
 get 'aboutus' => 'sites#aboutus'
 get '/contact' => 'sites#contactus'
+get '/howworks' => 'sites#howworks'
 get '/bookingmanagment' => 'bookings#managment'
  root to: 'sites#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415215509) do
+ActiveRecord::Schema.define(version: 20170416154431) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20170415215509) do
     t.boolean  "noaddons"
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "message"
   end
 
   create_table "profiles", force: :cascade do |t|
