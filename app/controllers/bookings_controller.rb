471c6481f6947305bfaa64aaf06054fdef65f7b8
Elianny Arias <arias.vzla@gmail.com>
2017-04-16 23:09:16 +0100

    contacts added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -40,7 +40,7 @@ class BookingsController < ApplicationController
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to new_charge_path
+      redirect_to paymentdone_path
   end
 
    
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 Rails.application.routes.draw do
   resources :contacts
   resources :profiles
-
+#devise was modified to control user sessions and registrations
 devise_for :users, controllers: {
         registrations: 'registrations/registrations',
         sessions: 'sessions/sessions'
       }
+       #routes for bikes
         resources :bikes
-
+        #routes for users
          resources :users do
            resources :bookings
           end
-            resources :charges
-       
-         
-        
-       
-         
 
-    get '/summary' => 'bookings#summary'
-  get '/stadistics' => 'profiles#stadistics'
+        resources :charges   
+# route for showing summary
+get '/summary' => 'bookings#summary'
+get '/paymentdone' => 'bookings#paymentdone'
+get '/stadistics' => 'profiles#stadistics'
 get '/profilepage' => 'profiles#userprofilepage'
 get '/home' => 'sites#index'
 get '/bookings' => 'profiles#adminpage'
@@ -28,7 +26,8 @@ get 'aboutus' => 'sites#aboutus'
 get '/contact' => 'sites#contactus'
 get '/howworks' => 'sites#howworks'
 get '/bookingmanagment' => 'bookings#managment'
- root to: 'sites#index'
+      #root path
+       root to: 'sites#index'
 
 
 end
